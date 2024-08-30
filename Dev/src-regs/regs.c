16705d266e1d933e9dc1bd4e7841567070c30bfe
Michael Mangelsdorf <mim@ok-schalter.de>
2024-08-30 07:39:43 +0200

    Add locals display

--- Dev/src-regs/regs.c
+++ Dev/src-regs/regs.c
@@ -23,7 +23,7 @@
 
 char* fname="corestate.myst";
 int i;
-
+int n;
 
 void
 main( int argc, char *argv[])
@@ -31,24 +31,30 @@ main( int argc, char *argv[])
         struct myth_vm vm;
         load( &vm, fname);
 
-        print( "LOX regs @l%.02X: ", vm.l);
+        print( "LOX regs: ", vm.l);
 
-        print("r:%.02Xh(%d)(%b) ", vm.r, vm.r, vm.r);
-        print("o:%.02Xh(%d)(%b) ", vm.o, vm.o, vm.o);
-        print("j:%.02Xh(%d)(%b) ", vm.j, vm.j, vm.j);
-        print("c:%.02Xh(%d)(%b) ", vm.c, vm.c, vm.c);
-        print("d:%.02Xh(%d)(%b) ", vm.d, vm.d, vm.d);
-        print("g:%.02Xh(%d)(%b) ", vm.g, vm.g, vm.g);
-        print("i:%.02Xh(%d)(%b)\n", vm.i, vm.i, vm.i);
+        print( "r:%.02Xh(%d)(%b) ", vm.r, vm.r, vm.r);
+        print( "o:%.02Xh(%d)(%b) ", vm.o, vm.o, vm.o);
+        print( "j:%.02Xh(%d)(%b) ", vm.j, vm.j, vm.j);
+        print( "c:%.02Xh(%d)(%b) ", vm.c, vm.c, vm.c);
+        print( "d:%.02Xh(%d)(%b) ", vm.d, vm.d, vm.d);
+        print( "g:%.02Xh(%d)(%b) ", vm.g, vm.g, vm.g);
+        print( "i:%.02Xh(%d)(%b)\n", vm.i, vm.i, vm.i);
 
-        print("\te:%.02Xh(%d)(%b) ", vm.e, vm.e, vm.e);
-        print("sclk:%d ", vm.sclk ? 1:0);
-        print("miso:%d ", vm.miso ? 1:0);
-        print("mosi:%d ", vm.mosi ? 1:0);
-        print("sir:%.02Xh(%d)(%b) ", vm.d, vm.d, vm.d);
-        print("sor:%.02Xh(%d)(%b) ", vm.g, vm.g, vm.g);
-        print("pir:%.02Xh(%d)(%b) ", vm.i, vm.i, vm.i);
-        print("por:%.02Xh(%d)(%b) ", vm.i, vm.i, vm.i);
+        print( "\te:%.02Xh(%d)(%b) ", vm.e, vm.e, vm.e);
+        print( "sclk:%d ", vm.sclk ? 1:0);
+        print( "miso:%d ", vm.miso ? 1:0);
+        print( "mosi:%d ", vm.mosi ? 1:0);
+        print( "sir:%.02Xh(%d)(%b) ", vm.d, vm.d, vm.d);
+        print( "sor:%.02Xh(%d)(%b) ", vm.g, vm.g, vm.g);
+        print( "pir:%.02Xh(%d)(%b) ", vm.i, vm.i, vm.i);
+        print( "por:%.02Xh(%d)(%b)\n", vm.i, vm.i, vm.i);
+
+        print( "Locals @l%.02X: ", vm.l);
+        for( i=0; i<8; i++){
+                n = vm.pagebyte[vm.l][0xF8+i];
+                print("L%d:%X(%d)(%b) ", i, n, n, n);
+        }
 
         print( "\n");
         exits( "");
